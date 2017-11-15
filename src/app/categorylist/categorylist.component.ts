d9ba5077f79623f7b36ab1783b64d7db48783414
Avinash Maricharla <avinashmari9@gmail.com>
2017-11-14 22:36:39 -0500

    Add Categorylist file step5

--- src/app/categorylist/categorylist.component.ts
+++ src/app/categorylist/categorylist.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-
+import {CategorylistService} from'./categorylist.service';
 @Component({
   selector: 'app-categorylist',
   templateUrl: './categorylist.component.html',
   styleUrls: ['./categorylist.component.css'],
-  encapsulation: ViewEncapsulation.None
 })
 export class CategorylistComponent implements OnInit {
-
-  constructor() { }
+  lists = []
+  constructor(private _categorylistService : CategorylistService) { }
 
   ngOnInit() {
+    this._categorylistService.getCategorylists()
+    .subscribe(resData => this.lists = resData);
   }
 
 }
--- src/app/categorylist/categorylist.service.ts
+++ src/app/categorylist/categorylist.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import {Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class CategorylistService {
-
-  constructor() { }
-
+  private _url = "api/data.json"
+  constructor(private _http:Http) {}
+  
+  getCategorylists(): Observable<any>{
+      return this._http.get(this._url).map((response: Response) => response.json());
+  }
 }
+  
